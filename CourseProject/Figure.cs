c2722e897a5e3f6c9612c83c072700761548a7ee
JuliyaK <y.kashlakova@yandex.ru>
2017-04-15 19:25:49 +0700

    Фигура

--- CourseProject/Figure.cs
+++ CourseProject/Figure.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Cell;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +11,16 @@ namespace CourseProject
         protected string Color;
         public Cell CurrentCell;
         public abstract void Walk();
+        public void Cheking()
+        {
+            if (x < 1 && x > 8 && y < 1 && y > 8)
+            {
+                throw new Exception("Выбранный диапазон не принадлежит шахматной доске");
+            }
+            if (newCell.CurrentFigure != null)
+            {
+                throw new Exception("Клетка не пустая");
+            }
+        }
     }
 }
