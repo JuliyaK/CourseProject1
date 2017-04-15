535d3099cd241779b9e36227d62f9df596951359
JuliyaK <y.kashlakova@yandex.ru>
2017-04-15 22:23:06 +0700

    Доска

--- CourseProject/Chessboard.cs
+++ CourseProject/Chessboard.cs
@@ -8,6 +8,18 @@ namespace CourseProject
 {
     class Chessboard : Cell
     {
-        //Наследуется класс Cell
+        public override void MakeCells(Cell ArrayCell)
+        {
+            int x = ArrayCell.CoordinateX;
+            int y = ArrayCell.CoordinateY;
+            int[,] arrayCells = new int[8, 8];
+            for (x = 1; x <= 8; x++)
+            {
+                for(y = 1; y <= 8; y++)
+                {
+                   // if()
+                }
+            }
+        }
     }
 }
