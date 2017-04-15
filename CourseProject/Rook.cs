2212eff1c9b019e0b24cc103f8506eaaff923e45
JuliyaK <y.kashlakova@yandex.ru>
2017-04-15 14:15:29 +0700

    Ладья

--- CourseProject/Rook.cs
+++ CourseProject/Rook.cs
@@ -14,38 +14,17 @@ namespace CourseProject
         {
             x = CoordinateX;
             y = CoodinateY;
-            try
+            if (CurrentCell.x + newCell.x < 1 && CurrentCell.x + newCell.x > 8 && CurrentCell.y + newCell.y < 1 && CurrentCell.y + newCell.y > 8)
             {
-                if (CurrentCell.x + newCell.x < 1 && CurrentCell.x + newCell.x > 8 && CurrentCell.y + newCell.y < 1 && CurrentCell.y + newCell.y > 8)
-                {
-                    throw new Exception("Выбранный диапазон не принадлежит шахматной доске");
-                }
+                throw new Exception("Выбранный диапазон не принадлежит шахматной доске");
             }
-            catch (Exception e)
+            if (CurrentFigure != null)
             {
-                Console.WriteLine("Ошибка: Неправильно выбранн диапозон!");
+                throw new Exception("Клетка не пустая");
             }
-            try
+            if (CurrentCell.x != newCell.x && CurrentCell.y != newCell.y)
             {
-                if (CurrentFigure != null)
-                {
-                    throw new Exception("Клетка не пустая");
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Ошибка: Клетка занята!");
-            }
-            try
-            {
-                if(CurrentCell.x != newCell.x && CurrentCell.y != newCell.y)
-                {
-                    throw new Exception("Ладья имеет другой алгоритм движения");
-                }
-            }
-            catch(Exception e)
-            {
-                Console.WriteLine("Ошибка: Ход невозможен");
+                throw new Exception("Ладья имеет другой алгоритм движения");
             }
             CurrentCell = newCell;
         }
